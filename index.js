e8a3eb9164dcbc55858a2a92156982a8b408381a
sabb12 <sabb0923@gmail.com>
2024-11-07 16:08:07 +0900

    html 구조, 수정 기능, Enter key 기능, 입력창 empty 경우 appen 불가, css 추가

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
-$("button").on("click", function () {
-  const inputValue = $("input").val();
-  const todoList = $("<div>").append(
+$("#addBtn").on("click", function () {
+  console.log("clicking")
+  const inputValue = $(".input").val();
+  if (inputValue.trim() === "") return;
+  
+  const todoList = $("<div>").attr({ class: "todoContainer", id: Date.now() }).append(
+    $("<div>").attr({class:"todoSection1"}).append(
     $("<input>").attr({
       type: "checkbox",
       class: "checkbox",
       checked: false,
+    })),
+    $("<div>").attr({class:"todoSection2"}).append(
+    $("<input>").attr({
+      type: "text",
+      class: "todo",
+      value: inputValue,
+      disabled: true
     }),
-    inputValue,
-    $("<button>").text("삭제").addClass("delBtn")
+    $("<button>").attr({class:"updateButton"}).text("수정"),
+    $("<button>").text("삭제").addClass("deleteButton")
+    ) 
   );
 
-  $(".todoList").append(todoList);
-  $("input").val("");
+  $(".todoContent").append(todoList);
+  $(".input").val("");
 
-  $(".delBtn").on("click", function () {
-    $(this).closest("div").remove();
+  $(document).on("click", ".updateButton", function () {
+    const $todoItem = $(this).closest(".todoContainer");
+    const $todoInput = $todoItem.find(".todo");
+  
+    if ($(this).text() === "수정") {
+      $(this).text("저장");
+      $todoInput.prop("disabled", false).focus();
+    } else if ($(this).text() === "저장") {
+      $(this).text("수정");
+      $todoInput.prop("disabled", true);
+    }
   });
 
-  $(".checkbox").on("click", function (e) {
+  $(document).on("click", ".deleteButton", function() {
+    const todoItemId = $(this).closest(".todoContainer").attr("id");
+    $("#" + todoItemId).remove();
+  });
+
+  $(document).on("click", ".checkbox", function (e) {
+    const $todoItem = $(this).closest(".todoContainer");
     if ($(this).prop("checked")) {
-      $(".completedList").append($(this).closest("div"));
+      $(".doneContent").append($todoItem);
+      $todoItem.find(".updateButton").css("display", "none")
     } else {
-      $(".todoList").append($(this).closest("div"));
+      $(".todoContent").append($todoItem);
+      $todoItem.find(".updateButton").css("display", "block")
+    }
+  });
+
+  $(".input").on("keydown", function (e) {
+    if (e.key === "Enter") {
+      $("#addBtn").click(); 
     }
   });
 });
